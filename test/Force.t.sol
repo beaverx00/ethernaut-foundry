dfb11ad0357cf0ebf580e9515a011219bf4fbe29
beaverx00 <beaverx00@gmail.com>
2023-04-07 17:25:59 +0900

    add Force test

---
+++ test/Force.t.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+import {ForceFactory} from "src/levels/ForceFactory.sol";
+import {ForceAttack} from "src/attacks/ForceAttack.sol";
+
+contract ForceTest is Test {
+    ForceFactory public level;
+    address public target;
+    address public player;
+
+    function setUp() public {
+        // ----------------------------------
+        // Register level
+        // ----------------------------------
+        level = new ForceFactory();
+        player = makeAddr("PLAYER");
+        target = level.createInstance(player);
+    }
+
+    function test_Force() public {
+        // ----------------------------------
+        // Initiate attack
+        // ----------------------------------
+        vm.deal(player, 1 ether);
+        vm.startPrank(player);
+
+        ForceAttack forceAttack = new ForceAttack{value: 42}(target);
+        forceAttack.pwn();
+
+        // ----------------------------------
+        // Validate
+        // ----------------------------------
+        bool success = level.validateInstance(payable(address(target)), player);
+        assertEq(success, true);
+    }
+}
