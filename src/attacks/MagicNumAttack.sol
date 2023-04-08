501633b554490bd16f8eb7cb4d01c8135c6c5caa
beaverx00 <beaverx00@gmail.com>
2023-04-08 13:07:04 +0900

    add MagicNum test

---
+++ src/attacks/MagicNumAttack.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface IMagicNum {
+    function setSolver(address _solver) external;
+}
+
+contract MagicNumAttack {
+    IMagicNum public target;
+
+    constructor(address _target) {
+        target = IMagicNum(_target);
+    }
+
+    function pwn() external {
+        /*
+           Runtime code: 0x602a60005260206000F3
+           -
+           PUSH1 42
+           PUSH1 0
+           MSTORE
+           PUSH1 32
+           PUSH1 0
+           RETURN
+        */
+        /*
+           Creation code: 0x69602a60005260206000F3600052600a6016F3
+           -
+           PUSH10 <Runtime Code> 
+           PUSH1 0
+           MSTORE
+           PUSH1 10 
+           PUSH1 22 
+           RETURN
+        */
+        address solver;
+        bytes memory bytecode = hex"69602a60005260206000F3600052600a6016F3";
+        assembly {
+            solver := create(0, add(bytecode, 0x20), mload(bytecode))
+        }
+        target.setSolver(solver);
+    }
+}
---
+++ test/MagicNum.t.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+import {MagicNumFactory} from "src/levels/MagicNumFactory.sol";
+import {MagicNumAttack} from "src/attacks/MagicNumAttack.sol";
+
+contract MagicNumTest is Test {
+    MagicNumFactory public level;
+    address public target;
+    address public player;
+
+    function setUp() public {
+        // ----------------------------------
+        // Create level instance
+        // ----------------------------------
+        level = new MagicNumFactory();
+        player = makeAddr("PLAYER");
+        target = level.createInstance(player);
+    }
+
+    function test_MagicNum() public {
+        // ----------------------------------
+        // Initiate attack
+        // ----------------------------------
+        vm.startPrank(player);
+
+        MagicNumAttack magicNumAttack = new MagicNumAttack(target);
+        magicNumAttack.pwn();
+
+        vm.stopPrank();
+
+        // ----------------------------------
+        // Validate
+        // ----------------------------------
+        bool success = level.validateInstance(payable(target), player);
+        assertEq(success, true);
+    }
+}
